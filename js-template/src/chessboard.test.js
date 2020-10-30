34682a8b0df6ce30861fd46b4d6651b047ef9d44
Christian Kluge <christian.kluge@software-developer.org>
2020-10-30 11:31:28 +0100

    #5 exercises 002-program-structures completed

--- js-template/src/chessboard.test.js
+++ js-template/src/chessboard.test.js
@@ -3,7 +3,9 @@ const signChar = '#';
 let output = '';
 const size = 8;
 
+// counting and cotrolling rows
 for (let i = 1; i <= size; i++) {
+  // counting and building strings in the row
   for (let j = 1; j <= size; j++) {
     if ((i + j) % 2 == 0) {
       output += spaceChar;
--- js-template/src/fizz-buzz.test.js
+++ js-template/src/fizz-buzz.test.js
@@ -1,15 +1,28 @@
 let i;
-let modThree = 'Fizz';
-let modFive = 'Buzz';
+let outputRemainThree = 'Fizz';
+let outputRemainFive = 'Buzz';
 
+/*
 for (i = 1; i <= 100; i++) {
   if (i % 3 != 0 && i % 5 != 0) {
     console.log(i);
   } else if (i % 3 == 0 && i % 5 == 0) {
-    console.log(modThree + modFive);
+    console.log(outputRemainThree + outputRemainFive);
   } else if (i % 5 == 0) {
-    console.log(modFive);
+    console.log(outputRemainFive);
   } else if (i % 3 == 0) {
-    console.log(modThree);
+    console.log(outputRemainThree);
   }
+}
+*/
+
+// solution with ternary operator
+for (i = 1; i <= 100; i++) {
+  i % 3 != 0 && i % 5 != 0
+    ? console.log(i)
+    : i % 3 == 0 && i % 5 == 0
+    ? console.log(outputRemainThree + outputRemainFive)
+    : i % 5 == 0
+    ? console.log(outputRemainFive)
+    : console.log(outputRemainThree);
 }
