ce65db5e5cdd31f1954875f064f8f34c8acdb5ad
Christian Kluge <christian.kluge@software-developer.org>
2020-10-30 15:42:11 +0100

    #5 exercises 003-functions completed

---
+++ js-template/src/003-bean-counting.test.js
@@ -0,0 +1,24 @@
+/* Write a function countChars that takes a string as its only argument and returns
+ * a number that indicates how many uppercase “  ” characters there are in the
+ * string.
+ *
+ * Next, write a function called countParam that behaves like countChars, except
+ * it takes a second argument that indicates the character that is to be counted
+ */
+
+let testString = 'Bouncing Bean Counting'; // -> result is 2 UpperCase 'B's
+
+const countParam = (str, char) => {
+  let counter = 0;
+  for (let i = 0; i <= str.length; i++) {
+    if (str[i] == char) {
+      counter++;
+    }
+  }
+
+  return console.log(' ' + char + ' is  ' + counter + ' time in String : ' + testString);
+};
+
+countParam(testString, 'N');
+countParam(testString, 'B');
+countParam(testString, 'n');
