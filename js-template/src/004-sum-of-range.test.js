063083e0c63b93f2874021591ea6574529de06e9
Christian Kluge <christian.kluge@software-developer.org>
2020-11-01 12:07:29 +0100

    #5 004-sum-of-range, solution 02

--- js-template/src/004-sum-of-range.test.js
+++ js-template/src/004-sum-of-range.test.js
@@ -1,10 +1,13 @@
 let arrResult = [];
 
-const range = (start, end) => {
-  for (let i = start; i <= end; i++) {
-    arrResult.push(i);
+const range = (start, end, step) => {
+  if (step === undefined || step == 0) {
+    for (let i = start; i <= end; i++) arrResult.push(i);
+  } else {
+    for (let i = start; i <= end; i += step) arrResult.push(i);
   }
-  console.log('range: ' + arrResult);
+
+  console.log('values : ' + arrResult);
   return arrResult;
 };
 
@@ -13,10 +16,10 @@ const sum = (arrResult) => {
   for (let value of arrResult) {
     addResult += value;
   }
-  console.log('adding: ' + addResult);
+  console.log('adding : ' + addResult);
   return addResult;
 };
 
-// console.log(sum(range(1, 10)));
+console.log(sum(range(-2, 10, 2)));
 
-console.log(sum(range(-1, 5)));
+// console.log(range(1, 10, 2));
